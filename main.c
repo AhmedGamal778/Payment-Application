4d2a8d93096b8bdb97a887bdfff423f7df144d50
AhmedGamal778 <85313647+AhmedGamal778@users.noreply.github.com>
2022-11-20 12:06:25 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include "Application/Application.h"
+#include "Server/Server.h"
+#include "Terminal/Terminal.h"
+#include "Card/Card.h"
+#include<stdio.h>
+
+
+int main ()
+{
+	typedef int arr [5];
+	arr iarr ={1,2,3,4,5};
+	int i ;
+	for (i=0 ; i<4 ; i++)
+		printf("%d," , iarr[i]);
+	//appStart();
+	//listSavedTransactionsTest();
+	// isValidAccountTest();
+	// isBlockedAccountTest();
+	 //isAmountAvailableTest();
+	 //recieveTransactionDataTest();
+	 //saveTransactionTest();
+	 //listSavedTransactionsTest();
+
+}
